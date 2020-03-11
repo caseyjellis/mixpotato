be527381979768defc33b9dacaf2b6f9f6616701
Rafael Vescovi <ravescovi@gmail.com>
2020-03-10 20:53:01 -0500

    final_result

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # mixpotato
 Mix potato is a classroom module for python shenanigans
 
+# final result 
+raf[('lettuce', '00')][('lettuce', '01')][('tomato', '00')][('tomato', '01')][('tomato', '02')]
+
 ## Installation
 
 1. python setup.py install
---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="mixp", 
+    version="0.0.2",
+    author="Rafael Vescovi",
+    author_email="ravescovi@gmail.com",
+    description="MSCA Class Package",
+    long_description_content_type="text/markdown",
+    url="https://github.com/ravescovi/mixpotatos",
+    packages=find_packages(exclude=["*test*"]),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
